2d7f1688d85f4ff7ec7e9022d1b522e02bbde959
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-18 14:49:58 +0500

    salom
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- index.js
+++ index.js
@@ -5,4 +5,36 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   loader()
 
+  const container = document.querySelector('.carousel__container');
+  let scrollAmount = 0;
+
+  document.querySelector('.carousel__button--right').addEventListener('click', () => {
+    container.scrollLeft += 200;
+  }, 2000);
+
+
+  function autoCarusel() {
+    return container.scrollLeft += 200;
+  }
+  setInterval(() => {
+    if (container.scrollLeft == 0) {
+      clearInterval(autoCarusel)
+      container.scrollLeft -= 200;
+    } else {
+      autoCarusel()
+    }
+  }, 2000);
+
+  setInterval(() => {
+    autoCarusel()
+  }, 3000)
+
+  document.querySelector('.carousel__button--left').addEventListener('click', () => {
+    container.scrollLeft -= 200;
+  });
+
+
 })
+
+
+
